50f25a2fb0a0181637e4574cb99f1cdde1b4af7d
Diego Andrés Lopez Rodriguez <dlopez258@estudiantes.areandina.edu.co>
2024-08-23 17:43:11 -0500

    Add files via upload

---
+++ PHP-HTML/Public/index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="src/style.css">
+    <title>Mi Proyecto PHP || Fc Barcelona </title>
+</head>
+<body>
+
+<section div class="escudo">
+    <img src="https://hips.hearstapps.com/hmg-prod/images/barcelona-1538072573.jpg" class="escudo">
+</section>
+
+<!-- video -->
+<section div class="video">
+    <iframe width="400" height="300" src="https://www.youtube.com/embed/b9H-Z7i0PQ0" title="Xavi manda un mensaje a la afición: &quot;Tú eres el Barça&quot; I MARCA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+</section>
+
+<section div class="estadio">
+<h1>Estadio del FC Barcelona</h1>
+<img src="https://estaticos-cdn.prensaiberica.es/clip/a99e1a8b-bb1b-43d2-ba95-6cc38bde2906_16-9-discover-aspect-ratio_default_0.jpg" width="300px" height="200px">
+<?php
+$estadio = "Spotify Camp Nou";
+echo "<p> " . $estadio . "</p>";
+?>
+
+<section div class="entrenador">
+    <h1>Entrenador del FC Barcelona</h1>
+    <img src="https://assets.laliga.com/squad/2024/t178/man52340/512x512/man52340_t178_2024_1_003_000.png" width="300px" height="300px">
+    <?php
+    $entrenador = "Hans-Dieter Flick";
+    echo "<p>" . $entrenador . "</p>";
+    ?>
+</section>
+
+    <h1>Plantilla </h1>
+<section div class="portero">
+
+    <h3>Ter Stegen </h3>
+    <img src="https://assets.laliga.com/squad/2024/t178/p77318/512x512/p77318_t178_2024_1_003_000.png">
+</section>
+
+    <section div class="defensas">
+
+        <h3>Pau Cubarsi </h3>
+        <img src="https://assets.laliga.com/squad/2024/t178/p593110/512x512/p593110_t178_2024_1_003_000.png">
+
+        <h3>Alejandro Balde </h3>
+        <img src="https://assets.laliga.com/squad/2024/t178/p493019/512x512/p493019_t178_2024_1_003_000.png">
+
+        <h3> Iñigo Martinez </h3>
+        <img src="https://assets.laliga.com/squad/2024/t178/p90318/512x512/p90318_t178_2024_1_003_000.png">
+
+        <h3> Andreas Christensen </h3>
+        <img src="https://assets.laliga.com/squad/2024/t178/p135363/512x512/p135363_t178_2024_1_003_000.png">
+
+    </section>
+
+    <section div class="centrocampistas">
+
+        <h3> Gavi </h3>
+        <img src="https://assets.laliga.com/squad/2024/t178/p500046/512x512/p500046_t178_2024_1_003_000.png">
+
+        <h3> Pedri </h3>
+        <img src="https://assets.laliga.com/squad/2024/t178/p490541/512x512/p490541_t178_2024_1_003_000.png">
+
+        <h3> Frenkie De Jong </h3>
+        <img src="https://assets.laliga.com/squad/2024/t178/p209712/512x512/p209712_t178_2024_1_003_000.png">
+
+    </section>
+
+    <section div class="delanteros">
+
+        <h3> Robert Lewandowski </h3>
+        <img src="https://assets.laliga.com/squad/2024/t178/p56764/512x512/p56764_t178_2024_1_003_000.png">
+
+        <h3> Raphinha </h3>
+        <img src="https://assets.laliga.com/squad/2024/t178/p219961/512x512/p219961_t178_2024_1_003_000.png">
+
+        <h3> Lamine Yamal </h3>
+        <img src="https://assets.laliga.com/squad/2024/t178/p593109/512x512/p593109_t178_2024_1_003_000.png">
+
+    </section>
+
+</body>
+</html>
---
+++ PHP-HTML/Public/src/inscripcion.php
@@ -0,0 +1,6 @@
+<?php
+
+echo "<h1>Formulario de inscripción</h1>";
+echo "<pre>";
+var_dump($_GET);
+echo "</pre>";
